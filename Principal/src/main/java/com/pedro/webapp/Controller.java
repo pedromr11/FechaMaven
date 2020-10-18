b6cca52744ca0e983a040fd62750071c49ec040e
Pedro Muñoz <pedromurivas11@gmail.com>
2020-10-18 18:27:44 +0200

    Ejercicio final

---
+++ Principal/src/main/java/com/pedro/webapp/Controller.java
@@ -0,0 +1,53 @@
+package com.pedro.webapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet("/Fecha")
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		out.print("<html> <body>");
+		
+		out.print("Esta es la fecha: " + new Date());
+		
+		out.print("</body> </html>");
+	}
+
+}
---
+++ Principal/src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h2>Pulsa en enviar para solicitar la fecha:</h2>
+	
+	<form action="http://localhost:8080/Principal/Fecha" method="post">
+	
+	<input type="submit" value="Enviar"> </input>
+	
+</body>
+</html>
